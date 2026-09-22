f659834ff1ceb452de7934a23265e69a12640fae
Mehedi Hasan <huzzat2@gmail.com>
2026-09-22 08:23:45 +0600

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,12 +1,14 @@
 import json
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi.responses import JSONResponse
 
 app = FastAPI(title="Mock API")
 
 with open("api/books.json", "r", encoding="utf-8") as f:
-    books_data = json.load(f)
+    data = json.load(f)
+
+books = data["books"]
 
 
 @app.get("/")
@@ -15,8 +17,38 @@ def index():
 
 
 @app.get("/books")
-def get_books():
-    return JSONResponse({"data": books_data["books"]})
+def get_books(
+    seacrh: str | None = None,
+    limit: int = Query(10, ge=1, le=100),
+    page: int = Query(1, get=1),
+):
+
+    results = books
+
+    # search
+    if seacrh:
+        seacrh = seacrh.lower()
+        results = [book for book in results if seacrh in book["title"].lower()]
+
+    # pagination
+    total = len(results)
+
+    start = (page - 1) * limit
+    end = start + limit
+
+    results = results[start:end]
+
+    return JSONResponse(
+        {
+            "items": results,
+            "pagination": {
+                "page": page,
+                "limit": limit,
+                "total": total,
+                "pages": (total + limit - 1) // limit,
+            },
+        }
+    )
 
 
 if __name__ == "__main__":
