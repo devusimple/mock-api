05c46482debb35737a8579656c1b601999279844
Mehedi Hasan <huzzat2@gmail.com>
2026-09-22 20:16:30 +0600

    add graphql features

--- api/main.py
+++ api/main.py
@@ -1,57 +1,24 @@
-import json
-
-from fastapi import FastAPI, Query
-from fastapi.responses import JSONResponse
-
-app = FastAPI(title="Mock API")
-
-with open("api/books.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
-
-books = data["books"]
+import strawberry
+from fastapi import FastAPI
+from strawberry.fastapi import GraphQLRouter
 
 
-@app.get("/")
-def index():
-    return JSONResponse("Welcome to the Mock API")
+@strawberry.type
+class Query:
+    @strawberry.field
+    def hello(self) -> str:
+        return "Hello World"
 
 
-@app.get("/books")
-def get_books(
-    seacrh: str | None = None,
-    limit: int = Query(10, ge=1, le=100),
-    page: int = Query(1, get=1),
-):
+schema = strawberry.Schema(Query)
 
-    results = books
+graphql_app = GraphQLRouter(schema)
 
-    # search
-    if seacrh:
-        seacrh = seacrh.lower()
-        results = [book for book in results if seacrh in book["title"].lower()]
-
-    # pagination
-    total = len(results)
-
-    start = (page - 1) * limit
-    end = start + limit
-
-    results = results[start:end]
-
-    return JSONResponse(
-        {
-            "items": results,
-            "pagination": {
-                "page": page,
-                "limit": limit,
-                "total": total,
-                "pages": (total + limit - 1) // limit,
-            },
-        }
-    )
+app = FastAPI()
+app.include_router(graphql_app, prefix="/graphql")
 
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app=app, host="0.0.0.0", reload=True)
+    uvicorn.run(app, reload=True)
